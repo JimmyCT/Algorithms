043ae0ebcb8ef1b1c4c72e92475cc05d61b2b23d
Jimmy Tran <jimmyctran2013@gmail.com>
2019-11-15 23:09:56 -0800

    Created Merge Intervals function
    Merge interval takes a list of intervals and sorts them by starting range number
    Once sorted, the intervals are merged as necessary and added to another list once it can no longer merge
    with other intervals

---
+++ Merge-Intervals.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+
+var merge = function(intervals) {
+  let output = [];
+  
+  if (intervals.length <= 1) {
+    return intervals;
+  }
+  
+  intervals.sort(function(firstInterval, secondInterval) {return firstInterval[0] - secondInterval[0]} );
+  
+  output.push(intervals[0]);
+  
+  let pointer = 0;
+  let prevEnd = intervals[0][1];
+  for (let i = 1; i < intervals.length; i++) {
+    const start = intervals[i][0];
+    const end = intervals[i][1];
+    
+    if (prevEnd >= start) {
+      const appendEnd = prevEnd >= end ? prevEnd : end;
+      output[pointer][1] = appendEnd;
+      prevEnd = appendEnd;
+    } else {
+      output.push([start, end]);
+      prevEnd = end;
+      pointer++
+    }
+  }
+
+  return output;
+};
