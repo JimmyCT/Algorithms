03e9549ccc1951aa23af40e45ec27954b7422527
Jimmy Tran <jimmyctran2013@gmail.com>
2019-11-19 10:51:53 -0800

    Added removeKdigits function
    -RemoveKdigits takes in a string num and number k and removes k digits to return the smallest number possible

--- Remove-K-Digits.js
+++ Remove-K-Digits.js
@@ -2,6 +2,32 @@
  remove k digits from the number so that the new number is the smallest possible.
 */
 
+function removeKdigits(num, k) {
+  let output = num;
+  
+  if (num.length <= k) {
+    return '0';
+  }
+  
+  for (let i = 0; i < k; i++) {
+    let j = 0;
+    while (output[j] <= output[j + 1] && j < output.length - 1) {
+      j++
+    }
+    
+    const left = removeLeadingZeros(output.substring(0, j));
+    const right = left.length === 0 ? removeLeadingZeros(output.substring(j + 1)) : output.substring(j + 1);
+    
+    output = left + right;
+  }
+  
+  if (output.length === 0) {
+    return "0";
+  }
+  
+  return output;
+};
+
 function removeLeadingZeros(str) {
   let output = ``;
   
