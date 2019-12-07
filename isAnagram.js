3466cd06b1d5aa17045e1c4bc4f32c4bdac9a89f
Jimmy Tran <jimmyctran2013@gmail.com>
2019-12-06 19:16:44 -0800

    created isAnagram function
    - isAnagram takes in two string arguments and checks if one string is a valid anagram of another

---
+++ isAnagram.js
@@ -0,0 +1,41 @@
+
+ function isAnagram(s, t) {
+  if (s.length !== t.length) {
+    return false;
+  }
+  
+  
+  const sCharsToCharCount = {size: 0};
+  const tCharsToCharCount = {size: 0};
+  
+  for (let i = 0; i < s.length; i++) {
+    if (sCharsToCharCount[s[i]] === undefined) {
+      sCharsToCharCount[s[i]] = 1
+      sCharsToCharCount.size++
+    } else {
+      sCharsToCharCount[s[i]]++
+    }
+  }
+  
+  for (let i = 0; i < t.length; i++) {
+    if (tCharsToCharCount[t[i]] === undefined) {
+      tCharsToCharCount[t[i]] = 1
+      tCharsToCharCount.size++
+    } else {
+      tCharsToCharCount[t[i]]++
+    }
+  }
+  
+  if (sCharsToCharCount.size !== tCharsToCharCount.size) {
+    return false;
+  }
+  
+  for (const tChar in tCharsToCharCount) {
+    if (sCharsToCharCount[tChar] === undefined || sCharsToCharCount[tChar] !== tCharsToCharCount[tChar]) {
+      return false;
+    }
+  }
+  
+  return true
+};
+
