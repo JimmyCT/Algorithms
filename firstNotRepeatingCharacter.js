2d6ee1a601603a48c4a6c75cdbd2a7f88e2baeec
Jimmy Tran <jimmyctran2013@gmail.com>
2019-12-11 19:26:11 -0800

    added firstNotRepeatingCharacter function
    -Func takes in a string and returns the first character in the string that is non-repeating
    -Algorithim is run in O(n) time complexity where n is the length of the string and O(1) space complexity

---
+++ firstNotRepeatingCharacter.js
@@ -0,0 +1,30 @@
+function firstNotRepeatingCharacter(s) {
+    if (s.length === 1) {
+        return s[0];
+    }
+    let minNonRepeatingIndex = s.length;
+    
+    const freq = new Array(26).fill(0);
+    const indicies = new Array(26).fill(-1);
+    for (let i = 0; i < s.length; i++) {
+        const charCode = s.charCodeAt(i) - 97;
+        
+        if (indicies[charCode] === -1) {
+            indicies[charCode] = i;
+        }
+        
+        freq[charCode]++;
+    }
+    
+    for (let i = 0; i < freq.length; i++) {
+        if (freq[i] === 1) {
+            minNonRepeatingIndex = minNonRepeatingIndex < indicies[i] ? minNonRepeatingIndex : indicies[i];
+        }
+    }
+    
+    if (minNonRepeatingIndex === s.length) {
+        return '_';
+    } else {
+        return s[minNonRepeatingIndex];
+    }
+}
