ca49b929c89ca93705d92ea1a1751d18b1e99125
Jimmy Tran <jimmyctran2013@gmail.com>
2019-12-17 17:08:33 -0800

    added compareVersion function
    - compareVersion takes in two string version numbers and returns
    - 1 if version1 is newer than version2
    - -1 if verison1 is older than version2
    - 0 otherwise

--- compareVersion.js
+++ compareVersion.js
@@ -8,3 +8,26 @@ function leadingZeroRemover(num) {
 
   return 0
 }
+
+/*returns 1 if version1 is newer than version2
+returns -1 if version1 is older than version2
+returns 0 otherwise
+*/
+function compareVersion(version1, version2) {
+  const version1Levels = version1.split('.');
+  const version2Levels = version2.split('.');
+  
+  const longestVersionLength = Math.max(version1Levels.length, version2Levels.length);
+  for (let i = 0; i < longestVersionLength; i++) {
+    let subV1 = version1Levels[i] === undefined ? 0 : parseInt(leadingZeroRemover(version1Levels[i]));
+    let subV2 = version2Levels[i] === undefined ? 0 : parseInt(leadingZeroRemover(version2Levels[i]));
+    
+    if (subV1 > subV2) {
+      return 1;
+    } else if (subV2 > subV1) {
+      return -1;
+    }
+  }
+  
+  return 0
+};
