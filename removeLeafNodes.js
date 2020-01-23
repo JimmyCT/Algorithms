ff54d30e2bee6ff599e49f7126e58f484ae6f6ea
Jimmy Tran <jimmyctran2013@gmail.com>
2020-01-22 22:52:34 -0800

    added removeLeafHelper function and completed removeLeafNodes function

--- removeLeafNodes.js
+++ removeLeafNodes.js
@@ -1,3 +1,24 @@
+function removeLeafHelper(currNode, target) {
+  if (currNode.val === target && currNode.left === null && currNode.right === null) {
+    currNode.val = null
+  } else {
+    if (currNode.left !== null) {
+      removeLeafHelper(currNode.left, target, currNode);
+      currNode.left = currNode.left.val === null ? null : currNode.left;
+    }
+    
+    if (currNode.right !== null) {
+      removeLeafHelper(currNode.right, target, currNode);
+      currNode.right = currNode.right.val === null ? null : currNode.right
+    }
+    
+    if (currNode.left === null && currNode.right === null && currNode.val === target) {
+      // removeLeafHelper(currNode, target);
+      currNode.val = null
+    }
+  }
+}
+
 function removeLeafNodes(root, target) {
   
   removeLeafHelper(root, target);
