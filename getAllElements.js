7dd417a2831c058e6a54591147c09aafe6dc5a8a
Jimmy Tran <jimmyctran2013@gmail.com>
2020-01-24 23:55:14 -0800

    Finished getAllElements function

--- getAllElements.js
+++ getAllElements.js
@@ -20,7 +20,22 @@ function fetchVals(root) {
 var getAllElements = function(root1, root2) {
   const root1Vals = fetchVals(root1);
   const root2Vals = fetchVals(root2);
-
+  
+  let pointer;
+  let root1Index = 0;
+  let root2Index = 0;
+  const output = [];
+  while (pointer !== null) {
+    if (root1Index === root1Vals.length && root2Index === root2Vals.length) {
+      pointer = null
+    } else if (root2Index === root2Vals.length || (root2Index !== root2Vals.length && root1Vals[root1Index] <= root2Vals[root2Index])) {
+      output.push(root1Vals[root1Index]);
+      root1Index++
+    } else {
+      output.push(root2Vals[root2Index])
+      root2Index++
+    }
+  }
   
   return output
 };
