8ca6963184c6189592f0792c1ac103d6b7d98c56
Jimmy Tran <jimmyctran2013@gmail.com>
2020-02-04 18:51:33 -0800

    added comments explaining the alphabetBoardPath function

--- alphabetBoardPath.js
+++ alphabetBoardPath.js
@@ -1,3 +1,8 @@
+//Given a target word to spell out on a board with the alphabet appearing in order on a in a grid with 
+//5 columns and the letter z by itself on the last row first column
+//We can only move in 4 directions, (L)eft, (U)p, (R)ight, (D)own
+//return a list of directions that can be taken starting at 'a' to spell out the target word 
+//in minimum moves 
 function alphabetBoardPath(target) {
   const boardCharToPositions = {};
   
