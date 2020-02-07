a15296b20ae9b2c7c64dc012b001a794b80a496b
Jimmy Tran <jimmyctran2013@gmail.com>
2020-02-06 19:11:03 -0800

    Created and finished allCellsDistanceOrder.js
    --Takes in a row and column length(r, c), as well as the row index and column index of a target coordinate
    --returns a list of the coordinates in the in the rXc matrix in order of distance from least to greatest
    --Distance is calculated using the Manhattan distance = |x_1 - x_0| + |y_1 - y_0|

---
+++ allCellsDistanceOrder.js
@@ -0,0 +1,32 @@
+function allCellsDistOrder(R, C, r0, c0) {
+  //treat r0 c0 as a target cord
+  //iterate through the RxC matrix and add the cordinnates to hashtable based on distance to target cord
+  //iterate from 0 to the max distance away from the target cord and add cords in that order to the output
+  
+  if (R === 0 || C === 0) {
+    return [];
+  }
+  
+  const distanceToCords = {};
+  let maxDist = 0;
+  for (let r = 0; r < R; r++) {
+    for (let c = 0; c < C; c++) {
+      const dist = Math.abs(r0 - r) + Math.abs(c0 - c);
+      maxDist = dist > maxDist ? dist : maxDist;
+      if (distanceToCords[dist] === undefined) {
+        distanceToCords[dist] = [[r, c]];
+      } else {
+        distanceToCords[dist].push([r, c]);
+      }
+    }
+  }
+  
+  const sortedCordsByDist = [];
+  for (let distance = 0; distance <= maxDist; distance++) {
+    for (let cords of distanceToCords[distance]) {
+      sortedCordsByDist.push(cords);
+    }
+  }
+  
+  return sortedCordsByDist;
+};
