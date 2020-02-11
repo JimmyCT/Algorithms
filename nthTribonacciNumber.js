bbf2c8376f29a39724bedc0adff1ed86892b5daf
Jimmy Tran <jimmyctran2013@gmail.com>
2020-02-10 22:42:11 -0800

    Added comments providing time and space complexity for nthTribonacciNumber

--- nthTribonacciNumber.js
+++ nthTribonacciNumber.js
@@ -1,6 +1,7 @@
 //Given T_0 = 1, T_=1, T_2 = 1, and T_n = T_n-3 + T_n-2 + T_n-1, n >= 0
-//this function returns the nth tribonacci number
+//this function returns the nth tribonacci number where n >= 0
 
+//Due to memoization, this function runs in O(n) time for n > 2 as well as O(n) storage
 function tribonacci(n) {
 	const memo = {0:0, 1:1, 2:1};
 
