d805708670a1f64909a21f87fcb51b19a4d8a37b
Jimmy Tran <jimmyctran2013@gmail.com>
2020-02-12 21:52:16 -0800

    added space and time complexity comments to maximum69Number function

--- maximum69Number.js
+++ maximum69Number.js
@@ -1,3 +1,5 @@
+
+//function runs in O(1) space complexity and O(n) time complexity where n is the number of digits in num
 function maximum69Number(num) {
   let stringNum = num.toString();
   for (let i = 0; i < stringNum.length; i++) {
