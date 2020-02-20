3e62bc67dbb72fe9401e2361c284e195db7193de
Jimmy Tran <jimmyctran2013@gmail.com>
2020-02-19 23:35:22 -0800

    added comments describing the zeroSum function as well as time and space analysis

--- nNumZeroSum.js
+++ nNumZeroSum.js
@@ -1,3 +1,6 @@
+//Given an integer n, this algorithim returns a list
+//of n numbers that sum up to 0
+//This algorithim runs in O(n) space and time complexity
 function sumZero(n) {
   const output = [];
   
